529351b6e3abc780b1d0c38e3bff69bbfb033f2b
Michael Schmit <m.schmit@pke.at>
2022-04-11 23:32:39 +0200

    test

--- modules/winccoaSystem/main.tf
+++ modules/winccoaSystem/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "winccoaSystem" {
     Name = "${var.winccoaSystemName}"
   }
   vpc_security_group_ids = [aws_security_group.ingress-all-ssh-winccoaSystem.id, aws_security_group.ingress-all-opcuaPort-winccoaSystem.id, aws_security_group.ingress-all-dbPort-winccoaSystem.id, aws_security_group.ingress-all-hotrod-winccoaSystem.id]
-  user_data = base64encode(templatefile("${path.module}/winccoa.tpl", { connectToOpcUaServers= var.connectToOpcUaServers, dbHost=var.dbHost, dbUser=var.dbUser, dbPassword=var.dbPassword, monitoringHost=var.monitoringHost, Grafana_Password=var.grafanaPassword } ))
+  user_data = base64encode(templatefile("${path.module}/winccoa.tpl", { connectToOpcUaServers= var.connectToOpcUaServers, dbHost=var.dbHost, dbUser=var.dbUser, dbPassword=var.dbPassword, monitoringHost=var.monitoringHost, grafanaPassword=var.grafanaPassword } ))
 }
 
 /************************SECURITY*****************************************/
